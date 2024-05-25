74069d3b46f52166a91c5879e4447124caa71eed
Bruno Muniz <brunomunizdev@gmail.com>
2024-05-25 20:17:18 -0300

    Add react router dom and add routers Home and About

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+//pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <h1>My Mini Blog Vite + React</h1>
+     <BrowserRouter>
+     <div className='container'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+     </div>
+     </BrowserRouter>
     </div>
   )
 }
