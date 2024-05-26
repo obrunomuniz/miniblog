4f249fdff9817ac352fa72dd2eea34f24961c2e7
Bruno Muniz <brunomunizdev@gmail.com>
2024-05-26 10:49:41 -0300

    Create layout page about and validate view pages nav

--- src/App.jsx
+++ src/App.jsx
@@ -41,13 +41,25 @@ function App() {
         <BrowserRouter>
           <Navbar />
           <div className='container'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/posts/create' element={<CreatePost />} />
-              <Route path='/dashboard' element={<Dashboard />} />
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
             </Routes>
           </div>
           <Footer />
