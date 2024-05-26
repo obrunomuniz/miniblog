ad953a56967510f2ee316f7cfd182f95783e375a
Bruno Muniz <brunomunizdev@gmail.com>
2024-05-26 12:19:44 -0300

    Create Detail Post

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,10 +1,34 @@
-import styles from "./Post.module.css"
+// CSS
+import styles from "./Post.module.css";
 
-export const Post = () => {
+// hooks
+import { useFetchDocument } from "../../hooks/useFetchDocument";
+import { useParams } from "react-router-dom";
+
+const Post = () => {
+  const { id } = useParams();
+  const { document: post } = useFetchDocument("posts", id);
+
   return (
-    <div>
-        <h1>Post</h1>
+    <div className={styles.post_container}>
+      {post && (
+        <>
+          <h1>{post.title}</h1>
+          <img src={post.image} alt={post.title} />
+          <p>{post.body}</p>
+          <h3>Este post trata sobre:</h3>
+          <div className={styles.tags}>
+            {post.tags.map((tag) => (
+              <p key={tag}>
+                <span>#</span>
+                {tag}
+              </p>
+            ))}
+          </div>
+        </>
+      )}
     </div>
-  )
-}
-export default Post
+  );
+};
+
+export default Post;
