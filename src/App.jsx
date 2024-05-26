eb67647436f5b24168243f3ecd184587c43dc1df
Bruno Muniz <brunomunizdev@gmail.com>
2024-05-26 15:55:15 -0300

    Ajust path deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import "./App.css";
-
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
-
-// hooks
-import { useState, useEffect } from "react";
-import { useAuthentication } from "./hooks/useAuthentication";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
 
 // pages
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Post from "./pages/Post/Post";
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Post from './pages/Post/Post';
 
 // components
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import CreatePost from "./pages/CreatePost/CreatePost";
-import Search from "./pages/Search/Search";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import EditPost from "./pages/EditPost/EditPost";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import CreatePost from './pages/CreatePost/CreatePost';
+import Search from './pages/Search/Search';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Dashboard from './pages/Dashboard/Dashboard';
+import EditPost from './pages/EditPost/EditPost';
 
 // context
-import { AuthProvider } from "./contexts/AuthContext";
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   const [user, setUser] = useState(undefined);
   const { auth } = useAuthentication();
-
   const loadingUser = user === undefined;
 
   useEffect(() => {
@@ -44,7 +39,7 @@ function App() {
   return (
     <div className="App">
       <AuthProvider value={{ user }}>
-        <BrowserRouter>
+        <BrowserRouter basename="/miniblog">
           <Navbar />
           <div className="container">
             <Routes>
